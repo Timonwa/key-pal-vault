7b895bb2c9d2ca2db026af583a008f72e8269c13
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-06-27 15:03:33 +0100

    boilerplate

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles/variables.scss";
+import "../styles/css-resets.scss";
+import "../styles/global.scss";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function Home() {
+  return (
+    <>
+      <h1>Login page</h1>
+      <Link href="/dashboard">Login</Link>
+    </>
+  );
+}
+
+export default Home;
