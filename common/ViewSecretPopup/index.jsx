341a525e561298ec800e2b91f18e0303c5b236e3
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-06-29 01:34:01 +0100

    card is ok

---
+++ common/ViewSecretPopup/index.jsx
@@ -0,0 +1,121 @@
+import styles from "./styles.module.scss";
+import useStore from "../../store";
+import { useEffect, useState } from "react";
+
+export function ViewSecretPopup({ onClose, handleView }) {
+  const userData = useStore((state) => state.userData);
+  const accountType = useStore((state) => state.accountType);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState("unapproved");
+  const [team, setTeam] = useState("None");
+  const [role, setRole] = useState("Member");
+
+  useEffect(() => {
+    if (userData?.name) {
+      setName(userData?.name);
+    }
+    if (userData?.email) {
+      setEmail(userData?.email);
+    }
+    if (userData?.status) {
+      setStatus(userData?.status);
+    }
+    if (userData?.team) {
+      setTeam(userData?.team);
+    }
+    if (userData?.role) {
+      setRole(userData?.role);
+    }
+  }, [userData]);
+
+  const data = {
+    name,
+    email,
+    status,
+    team,
+    role,
+  };
+
+  return (
+    <div className={styles.editMemberPopup}>
+      <h1 className={styles.title}>Edit Member Details</h1>
+
+      <form onSubmit={(e) => handleView(e, data)}>
+        <fieldset>
+          <label htmlFor="name">
+            Name
+            <input
+              id="name"
+              name="email"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              disabled
+            />
+          </label>
+
+          <label htmlFor="email">
+            Email
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              disabled
+            />
+          </label>
+
+          <label htmlFor="status">
+            Status
+            <select
+              id="status"
+              name="status"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              disabled={accountType === "Super Admin" ? false : true}>
+              <option value="approved">approved</option>
+              <option value="unapproved">unapproved</option>
+            </select>
+          </label>
+
+          <label htmlFor="team">
+            Team
+            <select
+              id="team"
+              name="team"
+              value={team}
+              onChange={(e) => setTeam(e.target.value)}>
+              <option value="Marketing">Marketing</option>
+              <option value="Development">Development</option>
+              <option value="None">None</option>
+            </select>
+          </label>
+
+          <label htmlFor="role">
+            Role
+            <select
+              id="role"
+              name="role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}>
+              <option value="Team Lead">Team Lead</option>
+              <option value="Member">Member</option>
+            </select>
+          </label>
+        </fieldset>
+
+        <div className={styles.buttons}>
+          <button className={styles.cancel} onClick={onClose}>
+            Cancel
+          </button>
+          <button className={styles.save} type="submit">
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- components/secrets/AllSecrets.jsx
+++ components/secrets/AllSecrets.jsx
@@ -1,23 +1,92 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import styles from "@/styles/secrets/AllSecrets.module.scss";
+import Modal from "@/common/Modal";
+import { DeletePopup } from "@/common/DeletePopup";
+import { ViewSecretPopup } from "@/common/ViewSecretPopup";
+import {
+  MdOutlineEditNote,
+  MdOutlineFilePresent,
+  MdPassword,
+} from "react-icons/md";
 
-export function SecretCard() {
+export function SecretCard({ item }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalType, setModalType] = useState("");
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+  const openModal = (type) => {
+    setIsOpen(true);
+    setModalType(type);
+  };
+
+  const handleView = (e, data) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    alert("deleted");
+  };
+
   return (
-    <article className={styles.secretCard}>
-      <h3 className={styles.secretTitle}>Secret Card title</h3>
+    <Fragment>
+      {modalType === "view" && (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ViewSecretPopup onClose={onClose} handleView={handleView} />
+        </Modal>
+      )}
+      {modalType === "delete" && (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <DeletePopup
+            type="Secret"
+            name="Secret card title"
+            onClose={onClose}
+            handleDelete={handleDelete}
+          />
+        </Modal>
+      )}
+      <article className={styles.secretCard}>
+        <div className={styles.secretTitleWrapper}>
+          <h3 className={styles.secretTitle}>Secret Card title</h3>
+          {item?.type === "password" && (
+            <p className={styles.type}>
+              <MdPassword />
+            </p>
+          )}
+          {item?.type === "note" && (
+            <p className={styles.type}>
+              <MdOutlineEditNote />
+            </p>
+          )}
+          {item?.type === "file" && (
+            <p className={styles.type}>
+              <MdOutlineFilePresent />
+            </p>
+          )}
+        </div>
 
-      <div className={styles.secretButtons}>
-        <button className={styles.viewBtn}>View</button>
-        <button className={styles.deleteBtn}>Delete</button>
-      </div>
-    </article>
+        <div className={styles.secretButtons}>
+          <button className={styles.viewBtn} onClick={() => openModal("view")}>
+            View
+          </button>
+          <button
+            className={styles.deleteBtn}
+            onClick={() => openModal("delete")}>
+            Delete
+          </button>
+        </div>
+      </article>
+    </Fragment>
   );
 }
 export default function AllSecrets({ data }) {
   return (
     <div className={styles.secretsGrid}>
       {data.map((item) => (
-        <SecretCard key={item.id} />
+        <SecretCard key={item.id} item={item} />
       ))}
     </div>
   );
